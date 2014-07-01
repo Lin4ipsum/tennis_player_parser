4337a93782e96be1e18664190a88f0c083cc9d8b
Lin4ipsum <lin.m.gonzalez@gmail.com>
2014-07-01 18:11:51 -0400

    snake case names

---
+++ models/player.rb
@@ -0,0 +1,14 @@
+class Player
+
+  attr_reader :last_name, :first_name, :gender, :date_of_birth, :favorite_color
+
+  def initialize(last_name, first_name, gender, date_of_birth, favorite_color)
+    @last_name       = last_name
+    @first_name      = first_name
+    @gender         = gender
+    @date_of_birth    = date_of_birth
+    @favorite_color  = favorite_color
+  end
+end
+
+
